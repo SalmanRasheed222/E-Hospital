9e133fe105ecc45a2c4e6f37e0650f91835ad87a
SALMAN RASHEED <SalmanR57@outlook.com>
2023-06-04 19:49:14 +0500

    Add project files.

---
+++ E-Hospital/Controllers/HospitalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Hospital.Controllers
+{
+    public class HospitalController : Controller
+    {
+        public IActionResult doctor()
+        {
+            return View();
+        }
+    }
+}
